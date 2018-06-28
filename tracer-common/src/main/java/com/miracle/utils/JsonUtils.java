4d489a5a422e15b885cdf7fbdb98389ffb8a6e4a
merorin <343932227@qq.com>
2018-06-28 16:36:25 +0800

    1.fastjson替换jackson 2.简单编写JsonUtils的工具类

--- tracer-common/src/main/java/com/miracle/utils/JsonUtils.java
+++ tracer-common/src/main/java/com/miracle/utils/JsonUtils.java
@@ -1,36 +1,23 @@
 package com.miracle.utils;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 
 import java.util.List;
 import java.util.Map;
 
 /**
  * Description:对JSON类型数据的工具类
- * 当前采用Jackson进行JSON的解析与序列化
+ * 当前采用fastjson进行JSON的解析与序列化
+ * 这个类的意义在于日后JSON底层实现替换时更为方便,只需改这个类
+ *
  *
  * @author guobin On date 2018/6/28.
  * @version 1.0
  * @since jdk 1.8
  */
 public class JsonUtils {
-
-    /**
-     * jackson的解析类
-     */
-    private static final ObjectMapper JACKSON_MAPPER;
-
-    /*
-     * 初始化jackson的解析类
-     */
-    static {
-        JACKSON_MAPPER = new ObjectMapper();
-        // 如果属性没有值，那么Json是会处理的，int类型为0，String类型为null，数组为[]，设置这个特性可以忽略空值属性
-        JACKSON_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-    }
 
     /**
      * 不允许实例化
@@ -43,39 +30,29 @@ public class JsonUtils {
      * @return JSON字符串,如果序列化出错会返回{@code null}
      */
     public static String toJSONString(Object obj) {
-        try {
-            return JACKSON_MAPPER.writeValueAsString(obj);
-        } catch (JsonProcessingException e) {
-            throw new JsonParseException(e);
-        }
+        return JSON.toJSONString(obj, SerializerFeature.DisableCircularReferenceDetect);
     }
 
     /**
      * 将一个JSON字符串解析成指定对象数据
      * @param jsonString json字符串
+     * @param valueType 元素对应的类型对象
      * @param <T> 指定对象的类型
      * @return 指定对象
      */
-    public static <T> T parseObject(String jsonString) {
-        try {
-            return JACKSON_MAPPER.readValue(jsonString, new TypeReference<T>(){});
-        } catch (Exception ex) {
-            throw new JsonParseException(ex);
-        }
+    public static <T> T parseObject(String jsonString, Class<T> valueType) {
+        return JSON.parseObject(jsonString, valueType);
     }
 
     /**
      * 将JSON字符串解析成{@link List},默认会解析成{@link java.util.ArrayList}
      * @param jsonString 字符串
+     * @param valueType 元素对应的类型对象
      * @param <E> list中的元素类型
      * @return 解析出来的list
      */
-    public static <E> List<E> parseList(String jsonString) {
-        try {
-            return JACKSON_MAPPER.readValue(jsonString, new TypeReference<List<E>>(){});
-        } catch (Exception ex) {
-            throw new JsonParseException(ex);
-        }
+    public static <E> List<E> parseList(String jsonString, Class<E> valueType) {
+        return JSON.parseArray(jsonString, valueType);
     }
 
     /**
@@ -85,25 +62,7 @@ public class JsonUtils {
      * @param <V> value的类型
      * @return 解析出来的map
      */
-    public static <K, V> Map<K, V> parseMap(String jsonString) {
-        try {
-            return JACKSON_MAPPER.readValue(jsonString, new TypeReference<Map<K, V>>(){});
-        } catch (Exception ex) {
-            throw new JsonParseException(ex);
-        }
-    }
-
-    /**
-     * JSON解析异常类,继承一个{@link RuntimeException}
-     */
-    private static class JsonParseException extends RuntimeException {
-        /**
-         * 提供一个默认构造器,防止某些框架解析或者构造对象失败
-         */
-        public JsonParseException() { }
-
-        JsonParseException(Throwable cause) {
-            super(cause);
-        }
+    public static <K, V> Map<K, V> parseMap(String jsonString, TypeReference<Map<K, V>> typeReference) {
+        return JSON.parseObject(jsonString, typeReference);
     }
 }
