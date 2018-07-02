ca2b0eceb845a3b1ab2bcff28debe5193a6398fd
merorin <343932227@qq.com>
2018-07-02 14:53:48 +0800

    1.重新实现时间片轮转算法 2.删除无用的类

--- tracer-common/src/main/java/com/miracle/round/RoundRobinDispatcher.java
+++ tracer-common/src/main/java/com/miracle/round/RoundRobinDispatcher.java
@@ -2,22 +2,18 @@ package com.miracle.round;
 
 import com.miracle.lock.LocalResourceLock;
 import com.miracle.lock.ResourceLock;
-import com.miracle.tree.BinaryTree;
-import com.miracle.tree.RedBlackTree;
 import com.miracle.utils.CollectionUtils;
-import com.miracle.utils.ListUtils;
-import com.miracle.utils.TreeUtils;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.ToLongFunction;
 
 /**
- * Description: 模拟时间片轮转调度算法
- * 采用红黑树作为数据的存储
+ * Description: 时间片轮转算法
  *
  * @author guobin On date 2018/6/29.
  * @version 1.0
@@ -28,7 +24,7 @@ public class RoundRobinDispatcher {
     /**
      * 默认时间片大小{@value}
      */
-    private static final long DEFAULT_TIME_SLICE = 100L;
+    static final long DEFAULT_TIME_SLICE = 100L;
 
     /**
      * 任务队列的资源锁
@@ -53,37 +49,31 @@ public class RoundRobinDispatcher {
     /**
      * 工作队列集合
      */
-    private final Map<String, TaskQueue<?>> taskQueueMap;
+    private final Map<String, TaskQueueDelegate<?>> taskQueueMap;
 
     /**
      * 每个队列与元素集合的对应关系
      */
     private final Map<String, Set<Object>> queueObjectsMap;
 
-    public RoundRobinDispatcher() {
+    /**
+     * 上一次的任务截止时间
+     */
+    private final AtomicLong lastDeadline;
+
+    RoundRobinDispatcher() {
         this(DEFAULT_TIME_SLICE);
     }
 
-    public RoundRobinDispatcher(long timeSlice) {
+    RoundRobinDispatcher(long timeSlice) {
         if (timeSlice <= 0) {
             throw new IllegalArgumentException("Time slice must greater that 0.");
         }
         this.timeSlice = timeSlice;
         this.taskQueueMap = new HashMap<>(16);
         this.queueObjectsMap = new HashMap<>(16);
-       SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(this::runPeriodically, 0, this.timeSlice, TimeUnit.MILLISECONDS);
-    }
-
-    /**
-     * 注册一个任务
-     * @param queueName 队列名字
-     * @param notification 任务过期时的通知任务
-     * @param <T> 任务中负载的数据类型
-     */
-    public <T> void registerTaskQueue(String queueName, ToLongFunction<T> notification) {
-        synchronized (this.taskQueueMap) {
-            this.taskQueueMap.putIfAbsent(queueName, new TaskQueue<>(queueName, notification));
-        }
+        this.lastDeadline = new AtomicLong(System.currentTimeMillis());
+        SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(this::runPeriodically, 0, this.timeSlice, TimeUnit.MILLISECONDS);
     }
 
     /**
@@ -92,22 +82,24 @@ public class RoundRobinDispatcher {
      * @param taskDuration 元素在时间片轮中的存活时长
      * @param queueName 工作队列名
      * @param <T> 元素的类型
+     * @throws NullPointerException 如果obj为{@code null}时抛出
      */
     @SuppressWarnings("unchecked")
-    public <T> void add(T obj, long taskDuration, String queueName) {
-        if (!this.taskQueueMap.containsKey(queueName)) {
-            throw new NullPointerException("The task queue which named," + queueName + ", has not been registered yet. "
-                    + "Please invoke registerTaskQueue() before you invoking add().");
+    public <T> void add(T obj, long taskDuration, String queueName, ToLongFunction<T> taskExecutor) {
+        if (obj == null) {
+            throw new NullPointerException("The object in the wheel cannot be null.");
         }
-        final long expireTime = System.currentTimeMillis() + taskDuration;
-        final Task<T> task = new Task<>(obj, expireTime, queueName);
+        final long taskCompleteTime = this.getTaskCompleteTime(taskDuration);
+        final String actualQueueName = this.buildActualQueueName(obj, queueName);
         QUEUE_RESOURCE_LOCK.lock(queueName);
         try {
-            final TaskQueue<T> taskQueue = (TaskQueue<T>) this.taskQueueMap.get(queueName);
-            this.queueObjectsMap.computeIfAbsent(queueName, k -> new HashSet<>()).add(task.obj);
-            taskQueue.offerTask(task);
+            TaskQueueDelegate<T> queue = (TaskQueueDelegate<T>) this.taskQueueMap.computeIfAbsent(actualQueueName,
+                    k -> new TaskQueueDelegate<>(actualQueueName, taskExecutor));
+            queue.offer(new Task<>(obj, taskCompleteTime, actualQueueName));
+            // 加入每个队列所对应的数据set中
+            this.queueObjectsMap.computeIfAbsent(actualQueueName, k -> new HashSet<>()).add(obj);
         } finally {
-            QUEUE_RESOURCE_LOCK.unlock(queueName);
+            QUEUE_RESOURCE_LOCK.unlock(actualQueueName);
         }
     }
 
@@ -131,79 +123,77 @@ public class RoundRobinDispatcher {
      */
     private void runPeriodically() {
         // 计算此次执行的过期时间
-        final long currentExpireTime = System.currentTimeMillis() + this.timeSlice;
-        this.taskQueueMap.values().forEach(taskQueue -> this.processOnTaskQueue(currentExpireTime, taskQueue));
+        this.lastDeadline.addAndGet(this.timeSlice);
+        this.taskQueueMap.values().forEach(this::processOnTaskQueue);
     }
 
     /**
      * 对任务队列所周期性执行的操作
-     * @param expireTime 当前=数据过期时间
      * @param taskQueue 任务队列
      * @param <T> 任务队列中数据的类型
      */
-    private <T> void processOnTaskQueue(long expireTime, TaskQueue<T> taskQueue) {
+    private <T> void processOnTaskQueue(TaskQueueDelegate<T> taskQueue) {
         QUEUE_RESOURCE_LOCK.lock(taskQueue.queueName);
         try {
-            final List<BinaryTree.Node<Long, Task<T>>> nodes = taskQueue.pollAllExpireNode(expireTime);
-            nodes.forEach(node -> this.processSubTree(node, taskQueue.notification));
+            this.runTaskAsync(taskQueue.poll(), taskQueue.notification);
         } finally {
             QUEUE_RESOURCE_LOCK.unlock(taskQueue.queueName);
         }
     }
 
-    /**
-     * 处理子树
-     * @param node 子树中的一个叶节点
-     * @param taskExecutor 任务执行器
-     * @param <T> 节点中任务持有的数据类型
-     */
-    private <T> void processSubTree(BinaryTree.Node<Long, Task<T>> node, ToLongFunction<T> taskExecutor) {
-        if (node == null) {
-            return;
-        }
-        final BinaryTree.Node<Long, Task<T>> rootNode = TreeUtils.findRootNode(node);
-        // 前遍历的方式遍历子树
-        TreeUtils.acceptPreOrderTraversal(rootNode, n -> this.processNode(n, taskExecutor));
-    }
-
-    /**
-     * 处理单个节点
-     * @param node 节点
-     * @param taskExecutor 任务执行器
-     * @param <T> 节点中任务持有的数据类型
-     */
-    private <T> void processNode(BinaryTree.Node<Long, Task<T>> node, ToLongFunction<T> taskExecutor) {
-        ListUtils.notNullOrEmptyList(node.values())
-                .forEach(task -> CompletableFuture.runAsync(() -> processTask(task, taskExecutor)));
-    }
-
-    /**
-     * 执行任务
-     * @param task 任务
-     * @param taskExecutor 任务执行器
-     * @param <T> 节点中任务持有的数据类型
-     */
     @SuppressWarnings("unchecked")
-    private <T> void processTask(Task<T> task, ToLongFunction<T> taskExecutor) {
-        // 不存在在queueObjectsMap中对应value(set)中的元素没有拿出来执行的必要,因为已经被删除任务了
-        final Set<T> objSet = (Set<T>) this.queueObjectsMap.get(task.queueName);
-        if (!CollectionUtils.isContaining(objSet, task.obj)) {
+    private <T> void runTaskAsync(Task<T> task, ToLongFunction<T> taskExecutor) {
+        // 如果在对应队列所持有的对象集中不存在所需要执行任务中持有的对象,说明这个任务已经被外界通过调用remove()取消掉了,不执行
+        Set<Object> queueObjectsSet = this.queueObjectsMap.get(task.queueName);
+        if (!CollectionUtils.isContaining(queueObjectsSet, task.obj)) {
             return;
         }
-        // 执行之前,将这个对象从任务队列所对应的objSet中删除
-        objSet.remove(task.obj);
-        long result = taskExecutor.applyAsLong(task.obj);
-        // result > 0说明任务没有异步执行成功,以result作为存活时间再度放回时间轮之中
-        if (result > 0) {
-            this.add(task.obj, result, task.queueName);
-        }
+
+        CompletableFuture.runAsync(() -> {
+            // 到这一步队列一定不会为null,所以直接用get获取
+            TaskQueueDelegate<T> queue = (TaskQueueDelegate<T>) this.taskQueueMap.get(task.queueName);
+            // 如果时间还没到完成时间,那么重新放回队列之中
+            if (task.completeTime > this.lastDeadline.get()) {
+                queue.offer(task);
+                return;
+            }
+            // 说明时间到了
+            final long result = taskExecutor.applyAsLong(task.obj);
+            // result > 0说明任务没有执行成功,以result作为任务工作时长再度放回时间轮之中
+            if (result > 0) {
+                Task<T> newTask = new Task<>(task.obj, this.getTaskCompleteTime(result), task.queueName);
+                queue.offer(newTask);
+            } else {
+                // 执行成功,将这个对象从任务队列所对应的objSet中删除
+                queueObjectsSet.remove(task.obj);
+            }
+        });
+    }
+
+    /**
+     * 根据参数类型和外界指定的任务队列名来生成一个在时间轮中实际应用的队列名
+     * @param obj 传入的对象
+     * @param outsideQueueName 外界指定的队列名
+     * @return 生成的实际队列名
+     */
+    private String buildActualQueueName(Object obj, String outsideQueueName) {
+        return obj.getClass().getSimpleName() + "-" + outsideQueueName;
+    }
+
+    /**
+     * 得到任务的完成时间
+     * @param taskNeedTime 任务执行所需时间
+     * @return 任务完成的时间戳
+     */
+    private long getTaskCompleteTime(long taskNeedTime) {
+        return System.currentTimeMillis() + taskNeedTime;
     }
 
     /**
      * 任务队列
-     * @param <T> 队列中每个任务所消费的数据类型
+     * @param <E> 队列中每个任务所消费的数据类型
      */
-    private static class TaskQueue<T> {
+    private static class TaskQueueDelegate<E> {
 
         /**
          * 队列名
@@ -213,36 +203,27 @@ public class RoundRobinDispatcher {
         /**
          * 存储任务的红黑树
          */
-        private final BinaryTree<Long, Task<T>> tree;
+        private final Queue<Task<E>> delegate;
 
         /**
          * 任务完成时调用的函数
          * 当函数返回值 > 0时会被重新以返回值作为过期时间放入时间片之中
          * 当函数返回值 = 0时视作任务完成
          */
-        private final ToLongFunction<T> notification;
+        private final ToLongFunction<E> notification;
 
-        TaskQueue(String queueName, ToLongFunction<T> notification) {
+        TaskQueueDelegate(String queueName, ToLongFunction<E> notification) {
             this.queueName = queueName;
             this.notification = notification;
-            this.tree = new RedBlackTree<>();
+            this.delegate = new LinkedList<>();
         }
 
-        /**
-         * 提交一份任务到任务队列之中
-         * @param task 任务
-         */
-        void offerTask(Task<T> task) {
-            this.tree.insert(task, t -> t.expireTime);
+        private void offer(Task<E> eTask) {
+            delegate.offer(eTask);
         }
 
-        /**
-         * 弹出所有过期失效的节点
-         * @param expireTime 过期时间
-         * @return 弹出的节点列表
-         */
-        List<BinaryTree.Node<Long, Task<T>>> pollAllExpireNode(long expireTime) {
-            return this.tree.popPreNodes(expireTime);
+        private Task<E> poll() {
+            return delegate.poll();
         }
     }
 
@@ -261,36 +242,17 @@ public class RoundRobinDispatcher {
         /**
          * 任务到期时间
          */
-        private final long expireTime;
+        private final long completeTime;
 
         /**
          * 任务所在队列名
          */
         private final String queueName;
 
-        Task(T obj, long expireTime, String queueName) {
+        Task(T obj, long completeTime, String queueName) {
             this.obj = obj;
-            this.expireTime = expireTime;
+            this.completeTime = completeTime;
             this.queueName = queueName;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (!(o instanceof Task)) {
-                return false;
-            }
-            Task<?> task = (Task<?>) o;
-            return expireTime == task.expireTime &&
-                    Objects.equals(obj, task.obj) &&
-                    Objects.equals(queueName, task.queueName);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(obj, expireTime, queueName);
         }
     }
 }
