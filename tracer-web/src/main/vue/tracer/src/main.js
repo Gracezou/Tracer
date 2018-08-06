de3c480393434a74afc4e96753db29dc2955d0e6
Gracezou <295057329@qq.com>
2018-08-06 12:06:45 +0800

    提交需求文档
    提交部分一期需求文档，提交vue框架

---
+++ tracer-web/src/main/vue/tracer/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+// import App from './App.vue'
+
+Vue.config.productionTip = false
+// 配置是否允许 vue-devtools 检查代码。开发版本默认为 true，生产版本默认为 false
+Vue.config.devtools = true
+// Vue.config.errorHandler = function(err, vm, info){
+// 	//todo
+// }
+
+// new Vue({
+//   render: h => h(App)
+// }).$mount('#app')
